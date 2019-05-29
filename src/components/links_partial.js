89336acbf69055f0e3ef747725ad660ba225d2ba
kilisio <pkilisio@gmail.com>
2019-05-29 11:08:29 +0300

    refactor(links_partial.js): edited test code

--- src/components/links_partial.js
+++ src/components/links_partial.js
@@ -28,7 +28,7 @@ export function links(...args){
 
 // // links_partial_view html object
 // export var links_partial_view_html = {
-//     div: links('file:///home/kilisio/google_drive/documentation/src/assets/online_portfolio/site_map.svg','site map image','active', 'testing')
+//     div: links('file:///home/kilisio/google_drive/documentation/src/assets/online_portfolio/site_map.svg', 'testing', null, null)
 // };
 //
 // // links_partial_view component
