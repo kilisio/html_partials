e5af4b70a0f79dcb42c527aaf88bb0badde145c7
kilisio <pkilisio@gmail.com>
2019-05-29 10:57:26 +0300

    fix(span_partial.js): enabled using null for undefined tag attributes

--- src/components/span_partial.js
+++ src/components/span_partial.js
@@ -7,8 +7,8 @@
 // span partial plugin
 export function span(...args){
     var obj = args[0];
-    var id = args[1];
-    var this_class = args[2];
+    var id = args[1] || '';
+    var this_class = args[2] || '';
     return{
         span: {
             _attrs:{
