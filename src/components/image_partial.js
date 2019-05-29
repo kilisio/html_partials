861b13ec0e8df8dd46f5d9066d7ce650b41767c4
kilisio <pkilisio@gmail.com>
2019-05-29 11:02:56 +0300

    fix(image_partial.js): enabled using null for undefined tag attributes

--- src/components/image_partial.js
+++ src/components/image_partial.js
@@ -5,8 +5,8 @@
 // image partial function
 export function image(...args){
     var src = args[0];
-    var alt_text = args[1];
-    var id = args[4];
+    var alt_text = args[1] || '""';
+    var id = args[2] || '""';
     return  '<img src=' + src + ' alt=' + alt_text + ' id=' + id + ' />';
 }
 
