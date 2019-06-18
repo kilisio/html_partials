330d499cf485e903bb8bd0ea3f779c8333f3b3dd
kilisio <pkilisio@gmail.com>
2019-06-18 14:23:11 +0300

    fix(image_partial.js): added class attribute

--- src/components/image_partial.js
+++ src/components/image_partial.js
@@ -7,7 +7,8 @@ export function image(...args){
     var src = args[0];
     var alt_text = args[1] || '""';
     var id = args[2] || '""';
-    return  '<img src=' + src + ' alt=' + alt_text + ' id=' + id + ' />';
+    var group = args[3] || '""';
+    return  '<img src=' + src + ' alt=' + alt_text + ' id=' + id + ' class=' + group + ' />';
 }
 
 
