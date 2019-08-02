92ce0c8ae2b4c0491fb062cd9f24e5342936a1f2
kilisio <pkilisio@gmail.com>
2019-08-02 10:59:43 +0300

    refactor(list_func.js): component test code

--- src/components/list_func.js
+++ src/components/list_func.js
@@ -1,12 +1,23 @@
 "use strict";
 
-// imports
+// IMPORTS
+//import * as css_plugins from "css_plugins_lib";
+//import * as html_partials from "html_partials_lib";
 
 
-// plugin_partial
-export function lists(...args){
-    var list = {
-        ul: []
+
+// // SCRIPTS
+// var eqcss = {
+// 	script:{
+// 		_attrs:{
+// 			src: './assets/scripts/eqcss/EQCSS.min.js'
+// 		}
+// 	}
+// };
+
+
+
+// FUNCTIONS
     };
     for(var i=0;i<args.length;i++){
         list.ul.push({ li: args[i] });
@@ -14,26 +25,72 @@ export function lists(...args){
     return list;
 }
 
-
-
-
-// // lists_partial html object
-// export var lists_partial_html = {
-//     div:[
-//         lists(
-//             'jane',
-//             'james',
-//             'john'
-//         )
+// // CSS VARIABLES
+//
+//
+//
+// // HTML VARIABLES
+// var inline_script = {
+//     'script[type="text/javascript"]':{
+//         _include:[
+//
+//         ]
+//     }	
+// };
+//
+// var inline_style = {
+//     style:{
+//         _include:[
+//             //css_plugins.scale('.list_func', 'landscape', 1366, 768),
+//             //css_plugins.scale('.list_func', 'portrait', 1366, 768)
+//         ]
+//     }
+// };
+//
+// var test = {
+//     id: "testings"
+// };
+//
+// // HTML OBJECT
+// export var list_func_html = {
+//     _include:[
+//         inline_style,
+//         inline_script,
+//         list(test, 'jane','austin','james','cameron')
 //     ]
 // };
 //
-// // lists_partial component
-// export var lists_partial = function(...args) {
+//
+//
+//// CSS OBJECT
+//export var list_func_base_css = {
+//};
+//export var list_func_component_css = {
+    //'@media (min-width: 769px) and (orientation: landscape)':{
+    //	margin: 0,
+    //	padding: 0
+    //},
+    //'@media (min-width: 769px) and (orientation: portrait)':{
+    //	margin: 0,
+    //	padding: 0
+    //},
+    //'@media (max-width: 768px) and (orientation: landscape)':{
+    //	margin: 0,
+    //	padding: 0
+    //},
+    //'@media (max-width: 768px) and (orientation: portrait)':{
+    //	margin: 0,
+    //	padding: 0
+    //}
+//};
+//
+// // COMPONENT
+// export function list_func(...args) {
 //     var nested_html_components = { _include: [] };
 //     if(args.length === 0){
-//             nested_html_components._include.push(lists_partial_html);
+//             nested_html_components._include.push(list_func_html);
 //     }else{
+//         nested_html_components._include.push(list_func_html);
 //         for(var i=0; i<args.length; i++) {
 //             nested_html_components._include.push(args[i]);
 //         }
@@ -41,34 +98,87 @@ export function lists(...args){
 //
 //     return {
 //         css:{
-//             '#lists_partial':[
-//
+//             '%base%': [
 //             ],
-//             '.lists_partial': {
-//                 margin: 0,
-//                 padding: 0
+//             '%component%.list_func': [
+//             ],
+//             '%%.list_func': {
+//                 '@media (min-width: 769px) and (orientation: landscape)':[
+//                 ],
+//                 '@media (min-width: 769px) and (orientation: portrait)':[
+//                 ],
+//                 '@media (max-width: 768px) and (orientation: landscape)':[
+//                 ],
+//                 '@media (max-width: 768px) and (orientation: portrait)':[
+//                 ]
+//             },
+//             '.list_func': {
+//                 '@media (min-width: 769px) and (orientation: landscape)':{
+//                     margin: 0,
+//                     padding: 0
+//                 },
+//                 '@media (min-width: 769px) and (orientation: portrait)':{
+//                     margin: 0,
+//                     padding: 0
+//                 },
+//                 '@media (max-width: 768px) and (orientation: landscape)':{
+//                     margin: 0,
+//                     padding: 0
+//                 },
+//                 '@media (max-width: 768px) and (orientation: portrait)':{
+//                     margin: 0,
+//                     padding: 0
+//                 }
+//             },
+//             span: {
+//                 '@media (min-width: 769px) and (orientation: landscape)':{
+//                     margin: 0,
+//                     padding: 0,
+//                     width: '100%',
+//                     display: 'inline-block'
+//                 },
+//                 '@media (min-width: 769px) and (orientation: portrait)':{
+//                     margin: 0,
+//                     padding: 0
+//                 },
+//                 '@media (max-width: 768px) and (orientation: landscape)':{
+//                     margin: 0,
+//                     padding: 0
+//                 },
+//                 '@media (max-width: 768px) and (orientation: portrait)':{
+//                     margin: 0,
+//                     padding: 0
+//                 }
 //             }
 //         },
 //         html:{
-//             'div#lists_partial.lists_partial': [
+//             'div#list_func.list_func': [
 //                 nested_html_components
 //             ]
 //
 //         }
-//
+//         
 //     };
+// }
+//     
+//
+// // HTML PAGE LAYOUT
+//
+// export var list_func_nested_layout = function(){
+// 	return list_func();
 // };
 //
-// // links_partial html page layout
-// var absurd = require("absurd");
-// var component_name = 'lists_partial';
+// var component_name = 'list_func';
+// //var page_title = '';
 //
-// //component imports
 //
-// export var lists_partial_layout = function(){
+// export function list_func_page_layout(){
 // 	return {
 // 		_:"<!DOCTYPE html>",
 // 		html: {
+// 			_attrs:{
+// 				style: "width:100%;height:100%;"
+// 			},
 // 			head: {
 // 				_include: [
 // 					'<link rel="stylesheet" type="text/css" href="' + component_name + '.css"/>'
@@ -79,30 +189,108 @@ export function lists(...args){
 // 				_attrs:{
 // 					style: "margin:0;padding:0;"
 // 				},
-// 				_include: lists_partial()
+// 				_include: [ 
+// 					list_func_nested_layout(),
+// 					eqcss
+// 				]
 // 			}
 // 		}
 // 	};
-// };
+// }
+//
+//
+// // ABSURD JS CONSOLE AND FILE TEST CODE
+// var absurd = require("absurd-0.3.158");
+//
 //
 // absurd(function(api) {
-// 	// print to file
-// 	var css_file = "./" + component_name + ".css";
-// 	var html_file = "./" + component_name + ".html";
-// 	api.morph("html").add(lists_partial_layout()).compileFile(html_file);
-// 	api.morph("component").add(lists_partial_layout()).compileFile(css_file);
+// 	// // print to file
+// 	// var css_file = "./" + component_name + ".css";
+// 	// var html_file = "./" + component_name + ".html";
+// 	// api.morph("html").add(list_func_page_layout()).compileFile(html_file);
+// 	// api.morph("component").add(list_func_page_layout()).compileFile(css_file);
 //
-// 	 // print to console
-// 	// component
-// 	api.morph("component").add(lists_partial()).compile(function(err, css, html) {
-// 		console.log(css);
-// 		console.log(html);
-// 	});
+// 	// print to console
+// 	// // component
+// 	// api.morph("component").add(list_func()).compile(function(err, css, html) {
+// 	//		console.log(css);
+// 	//		console.log(html);
+// 	// });
 // 	// component layout
-// 	api.morph("component").add(lists_partial_layout()).compile(function(err, css) {
+// 	api.morph("component").add(list_func_page_layout()).compile(function(err, css) {
 // 		console.log(css);
 // 	});
-// 	api.morph("html").add(lists_partial_layout()).compile(function(err, html) {
+// 	api.morph("html").add(list_func_page_layout()).compile(function(err, html) {
 // 		console.log(html);
 // 	});
 // });
+
+
+
+// // EXPRESS MIDDLEWARES
+// var list_func_page_css, list_func_page_html;
+//
+// absurd().morph("component").add(list_func_page_layout()).compile(function(err, css) {
+// 	   list_func_page_css = css;
+// });
+//
+// absurd().morph("html").add(list_func_page_layout()).compile(function(err, html) {
+// 	   list_func_page_html = html;
+// });
+//
+// export function list_func_css_mid(req, res){
+// 	res.send(list_func_page_css);
+// } 
+//
+// export function list_func_html_mid(req, res){
+// 	res.send(list_func_page_html);
+// } 
+//
+//
+//
+// // EXPRESS TEST SERVER
+//
+// // server imports
+// var express = require('express');
+// var helmet = require('helmet');
+// var path = require('path');
+//
+//
+// // global express server variables
+// var app = express();
+//
+// var setContentType = function(type) {
+// 	   return function(req, res, next) {
+// 		   res.setHeader('Content-Type', 'text/' + type);
+// 		   next();
+// 	   };
+// };
+//
+// // execute helmet before responding to requests
+// app.use(helmet());
+//
+// //set default path to web app resources
+// app.use('/assets',  express.static(path.join( __dirname, 'assets' )));
+// app.use('/db',  express.static(path.join( __dirname, 'db' )));
+// app.use('/log',	express.static(path.join( __dirname, 'log' )));
+//
+// // web application routes
+// app.get('/', setContentType("html"), list_func_html_mid);
+//
+// // list_func component specific routes
+// app.get('/list_func.css', setContentType("css"), list_func_css_mid);
+// app.get('/list_func.html', setContentType("html"), list_func_html_mid);
+//
+//
+// // start server
+// var port = process.env.PORT || 8080;
+// app.listen(port);
+// console.log('listening on port ' + port);
+//
+//
+// // open in browser
+// const open_browser = require('open');
+//
+// (async () => {
+// 	await open_browser('http://127.0.0.1:8080', {app: ['firefox', '-new-tab']});
+// })();
