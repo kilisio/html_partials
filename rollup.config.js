8326c957e5932fcf8e4956976c16fed65257a861
kilisio <pkilisio@gmail.com>
2019-10-09 22:11:06 +0300

    build(rollup.config.js): imported and added new plugin configurations

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import clear from 'rollup-plugin-clear';
 import cleanup from 'rollup-plugin-cleanup';
 import copy from 'rollup-plugin-copy';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 
 export default {
     input: 'src/lib.js',
@@ -12,12 +16,27 @@ export default {
         clear({
             targets: ['lib'],
         }),
+        resolve(),
+        commonJS({
+              include: 'node_modules/**'
+        }),
         cleanup(),
+        terser(),
+        generatePackageJson({
+            baseContents: {
+                "main": "lib/lib.js",
+            }
+        }),
         copy({
             targets: [
-                'src/assets',
-                'src/db',
-                'src/log'
+                {
+                    src: [
+                        'src/assets',
+                        'src/db',
+                        'src/log',
+                    ], 
+                    dest: 'lib'
+                }
             ]
         })
     ]
