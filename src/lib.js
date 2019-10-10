19986b27ca8ad3469906d834e5e3c4d3b02a7c10
kilisio <pkilisio@gmail.com>
2019-10-10 16:07:41 +0300

    fix(lib.js): changed static table functions export source file

--- src/lib.js
+++ src/lib.js
@@ -2,6 +2,6 @@
 export { ul_list, ol_list } from './list_func.js';
 export { link } from "./link_func.js";
 export { image } from "./image_func.js";
-export { static_table, static_row, static_cell } from "./static_table_partial.js";
+export { static_table, static_row, static_cell } from "./static_table_func.js";
 export { span } from "./span_func.js";
 export { div } from "./div_func.js";
