c2acd3ed489a55185bfb3882735dcb82d03e3cf6
kilisio <pkilisio@gmail.com>
2019-12-30 11:42:41 +0300

    fix(lib.js): exported normal html link

--- src/lib.js
+++ src/lib.js
@@ -5,3 +5,4 @@ export { image } from "./image_func.js";
 export { static_table, static_row, static_cell } from "./static_table_func.js";
 export { span } from "./span_func.js";
 export { div } from "./div_func.js";
+export { link } from "./func_link.js";
