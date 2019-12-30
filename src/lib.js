e7fbbcf04e823fc3cb244d8f1a3533b2e47ded91
kilisio <pkilisio@gmail.com>
2019-12-30 11:37:40 +0300

    chore: moved file from link_func.js to func_router_link.js

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
 
 export { ul_list, ol_list } from './list_func.js';
-export { link } from "./link_func.js";
+export { router_link } from "./func_router_link.js";
 export { image } from "./image_func.js";
 export { static_table, static_row, static_cell } from "./static_table_func.js";
 export { span } from "./span_func.js";
